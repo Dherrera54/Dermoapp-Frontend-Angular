1bf420bc7ab21736c084bbef653515b70f5036af
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-01-31 18:39:51 -0500

    Update to medic service spec

--- src/app/medic/medic.service.spec.ts
+++ src/app/medic/medic.service.spec.ts
@@ -1,13 +1,25 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, async, inject, getTestBed } from '@angular/core/testing';
 import { MedicService } from './medic.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('Service: Medic', () => {
+  let injector: TestBed;
+  let service: MedicService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [MedicService]
     });
+    injector = getTestBed();
+    service = injector.get(MedicService);
+    httpMock = injector.get(HttpTestingController);
+  });
+  afterEach(() => {
+    httpMock.verify({ignoreCancelled: true});
   });
 
   it('should ...', inject([MedicService], (service: MedicService) => {
