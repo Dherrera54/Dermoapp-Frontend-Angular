de1e80f14ed5a7ab96f65cea35018808ae4629c7
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-01-30 22:31:57 -0500

    Added internationalization english

--- src/app/medic/medic-singup/medic-singup.component.ts
+++ src/app/medic/medic-singup/medic-singup.component.ts
@@ -25,7 +25,8 @@ export class MedicSingupComponent implements OnInit {
     private medicService:MedicService,
     private formBuilder: FormBuilder,
     private routerPath: Router,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+
 
   ) { }
 
@@ -33,7 +34,7 @@ export class MedicSingupComponent implements OnInit {
 
    this.medicForm = this.formBuilder.group({
       email: ["", [Validators.required, Validators.email, Validators.maxLength(50)]],
-      password: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(4)]],
+      password: ["", [Validators.required, Validators.maxLength(50), Validators.minLength(8)]],
       confirmPassword: ["", [Validators.required]],
       name: ["", [Validators.required, Validators.maxLength(50)]],
       lastName: ["", [Validators.required, Validators.maxLength(50)]],
--- src/app/medic/medic.module.ts
+++ src/app/medic/medic.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule} from '@angular/material/checkbox';
 
 
 @NgModule({
@@ -18,7 +19,9 @@ import { MatSelectModule } from '@angular/material/select';
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatCheckboxModule
+
   ],
 
   declarations: [
