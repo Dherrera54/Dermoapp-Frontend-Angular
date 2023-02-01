c93c32b582e815f6ac4c1bdab653d474928b4dda
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-01-31 19:05:38 -0500

    Update to medic signup component spec

--- src/app/medic/medic-singup/medic-singup.component.spec.ts
+++ src/app/medic/medic-singup/medic-singup.component.spec.ts
@@ -4,14 +4,36 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { MedicSingupComponent } from './medic-singup.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {RouterTestingModule} from '@angular/router/testing';
+import { Router, ActivatedRoute } from '@angular/router';
+import {Observable, of} from 'rxjs';
 
 describe('MedicSingupComponent', () => {
   let component: MedicSingupComponent;
   let fixture: ComponentFixture<MedicSingupComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MedicSingupComponent ]
+      declarations: [ MedicSingupComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate'),
+          }},
+
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              snapshot: {params: {id: 100}}
+            }
+          },
+
+      ]
+
     })
     .compileComponents();
   }));
