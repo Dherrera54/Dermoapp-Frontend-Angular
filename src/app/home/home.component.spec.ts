62861ba3973f287d76ef4fa6aedee7eaf1d2ecb9
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-01-31 19:02:21 -0500

    Update to home  component spec

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,14 +4,37 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {RouterTestingModule} from '@angular/router/testing';
+import { Router, ActivatedRoute } from '@angular/router';
+import {Observable, of} from 'rxjs';
+
+
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate'),
+          }},
+
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              snapshot: {params: {id: 100}}
+            }
+          },
+
+      ]
     })
     .compileComponents();
   }));
