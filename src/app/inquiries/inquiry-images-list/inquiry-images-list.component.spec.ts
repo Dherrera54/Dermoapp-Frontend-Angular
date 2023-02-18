a67f6e39cedc89ffdad5bacc4ee084385ef5ba51
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-02-17 22:05:22 -0500

    updated inquiry images spec

--- src/app/inquiries/inquiry-images-list/inquiry-images-list.component.spec.ts
+++ src/app/inquiries/inquiry-images-list/inquiry-images-list.component.spec.ts
@@ -4,14 +4,36 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { InquiryImagesListComponent } from './inquiry-images-list.component';
-
+import {RouterTestingModule} from '@angular/router/testing';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 describe('InquiryImagesListComponent', () => {
   let component: InquiryImagesListComponent;
   let fixture: ComponentFixture<InquiryImagesListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InquiryImagesListComponent ]
+      declarations: [ InquiryImagesListComponent ],
+      imports:[HttpClientTestingModule, RouterTestingModule,  HttpClientModule,SharedModule, TranslateModule.forRoot()],
+      providers: [
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate'),
+          }},
+
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              snapshot: {params: {id: 100}}
+            }
+          },
+
+      ]
     })
     .compileComponents();
   }));
