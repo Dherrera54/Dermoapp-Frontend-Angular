5b117d233c6e02890fdd9aab4b69fdc34bf5451b
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-02-18 21:15:12 -0500

    updated medic sign up  spec

--- src/app/medic/medic-singup/medic-singup.component.spec.ts
+++ src/app/medic/medic-singup/medic-singup.component.spec.ts
@@ -4,6 +4,7 @@ import { asNativeElements, DebugElement } from '@angular/core';
 
 import { MedicSingupComponent } from './medic-singup.component';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpErrorResponse } from '@angular/common/http';
 import {RouterTestingModule} from '@angular/router/testing';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl } from '@angular/forms';
@@ -11,18 +12,29 @@ import { ToastrService, ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {  TranslateModule } from '@ngx-translate/core';
+import { MedicService } from '../medic.service';
+import { of } from 'rxjs';
 
 
 describe('MedicSingupComponent', () => {
   let component: MedicSingupComponent;
   let fixture: ComponentFixture<MedicSingupComponent>;
   let debug: DebugElement;
+  let medicService: jasmine.SpyObj<MedicService>;
+  let toastrService: jasmine.SpyObj<ToastrService>;
+
 
   beforeEach(async(() => {
+    const medicServiceSpy = jasmine.createSpyObj('MedicService', ['userSignUp', 'medicCreate', 'imgUpload']);
+    const toastrServiceSpy = jasmine.createSpyObj('ToastrService', ['success', 'error']);
+
     TestBed.configureTestingModule({
       declarations: [ MedicSingupComponent ],
       imports: [HttpClientTestingModule, RouterTestingModule, ToastrModule.forRoot(), HttpClientModule,SharedModule, TranslateModule.forRoot()],
       providers: [
+        FormBuilder,
+        { provide: MedicService, useValue: medicServiceSpy },
+        { provide: ToastrService, useValue: toastrServiceSpy },
         {
           provide: Router,
           useValue: {
@@ -42,6 +54,8 @@ describe('MedicSingupComponent', () => {
 
     })
     .compileComponents();
+    medicService = TestBed.inject(MedicService) as jasmine.SpyObj<MedicService>;
+    toastrService = TestBed.inject(ToastrService) as jasmine.SpyObj<ToastrService>;
   }));
 
   beforeEach(() => {
@@ -53,14 +67,24 @@ describe('MedicSingupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('test a form group elemnte count',()=>{
+  it('test a form group elemnt count',()=>{
     const FormElement = fixture.debugElement.nativeElement.querySelector('#medicForm');
     const inputElements = FormElement.querySelectorAll('input');
     expect(inputElements.length).toEqual(8); })
+    it('should show error when userSignUp fails', () => {
+      const errorResponse = new HttpErrorResponse({ status: 500, statusText: 'Internal Server Error' });
+      medicService.userSignUp.and.returnValue(of(errorResponse));
+      component.registerMedic();
+      expect(toastrService.error).toHaveBeenCalledWith(`Ha ocurrido un error: ${errorResponse.message}`, 'Error');
+    });
 
-  /*it('CHECK LOGH IN FORM IS VALID WHE VALIDATIONS ARE FULFILLED',()=>{
-    const createMedicFormElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#medicForm');
-    const inputElements = FormElement.querySelectorAll('input');
-    expect(inputElements.length).toEqual(8); })*/
+    it('should show success message when userSignUp succeeds', () => {
+      const response = { id: 1, email: 'test@test.com' };
+      medicService.userSignUp.and.returnValue(of(response));
+      component.registerMedic();
+      expect(toastrService.success).toHaveBeenCalledWith('Perfil de usuario creado con exito', 'Registro exitoso');
+    });
+
+
 
 });
