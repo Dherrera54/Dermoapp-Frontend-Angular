dc8371566ebcf3bee617f244d562717f791565fe
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-03-01 18:51:18 -0500

    Updated inquiry service spec

--- src/app/inquiries/inquiry.service.spec.ts
+++ src/app/inquiries/inquiry.service.spec.ts
@@ -8,10 +8,15 @@ import { SharedModule } from '../shared/shared.module';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { environment } from 'src/environments/environment';
 
+
+
 describe('Service: Inquiry', () => {
   let service: InquiryService;
   let httpMock: HttpTestingController;
+  let backUrl: string = environment.URL_PRODUCTION
+
   beforeEach(() => {
+
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,RouterTestingModule,SharedModule, HttpClientTestingModule
@@ -56,5 +61,29 @@ describe('Service: Inquiry', () => {
     expect(req.request.method).toBe('GET');
     req.flush(mockData);
   });
+
+  it('should return an array of inquiries by specialty', () => {
+    const specialty = 'exampleSpecialty';
+    const token = 'exampleToken';
+
+    const expectedInquiries = [
+      { id: 1, title: 'Example Inquiry 1' },
+      { id: 2, title: 'Example Inquiry 2' },
+      { id: 3, title: 'Example Inquiry 3' }
+    ];
+
+    service.getInquiriesBySpecialty(specialty, token).subscribe(inquiries => {
+      expect(inquiries).toEqual(expectedInquiries);
+    });
+
+    const req = httpMock.expectOne(`${backUrl}/consultations/specialty/${specialty}`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.headers.has('Authorization')).toBeTruthy();
+    expect(req.request.headers.get('Authorization')).toBe(`Bearer ${token}`);
+
+    req.flush(expectedInquiries);
+  });
+
+
 
 });
