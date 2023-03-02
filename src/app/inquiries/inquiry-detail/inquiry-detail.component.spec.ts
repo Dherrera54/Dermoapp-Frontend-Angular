d37491284a0739b31ce64fe426929f09f5bb40ed
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-03-01 20:16:30 -0500

    Updated inquiri detail specs

--- src/app/inquiries/inquiry-detail/inquiry-detail.component.spec.ts
+++ src/app/inquiries/inquiry-detail/inquiry-detail.component.spec.ts
@@ -1,5 +1,114 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Inquiry } from '../inquiriy';
+import { InquiryDetailComponent } from './inquiry-detail.component';
+import { MedicService } from '../../medic/medic.service';
+import { InquiryService } from '../inquiry.service';
+import { ToastrService } from 'ngx-toastr';
+import { of } from 'rxjs';
+import { Patient } from 'src/app/shared/models/patient';
+
+describe('InquiryDetailComponent', () => {
+  let component: InquiryDetailComponent;
+  let fixture: ComponentFixture<InquiryDetailComponent>;
+  let mockRouter: jasmine.SpyObj<Router>;
+  let mockActivatedRoute: jasmine.SpyObj<ActivatedRoute>;
+  let mockMedicService: jasmine.SpyObj<MedicService>;
+  let mockInquiryService: jasmine.SpyObj<InquiryService>;
+  let mockToastrService: jasmine.SpyObj<ToastrService>;
+
+  beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', [], {
+      snapshot: {
+        params: {
+          userToken: 'userToken',
+          medicId: 'medicId',
+          medicSpecialty: 'specialty',
+        },
+      },
+    });
+    const medicServiceSpy = jasmine.createSpyObj('MedicService', ['getMedicInquiriesById', 'addInquiryToMedic']);
+    const inquiryServiceSpy = jasmine.createSpyObj('InquiryService', ['getInquiryImage']);
+    const toastrServiceSpy = jasmine.createSpyObj('ToastrService', ['success', 'error']);
+
+    await TestBed.configureTestingModule({
+      declarations: [InquiryDetailComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteSpy },
+        { provide: MedicService, useValue: medicServiceSpy },
+        { provide: InquiryService, useValue: inquiryServiceSpy },
+        { provide: ToastrService, useValue: toastrServiceSpy },
+      ],
+    }).compileComponents();
+
+    mockRouter = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    mockActivatedRoute = TestBed.inject(ActivatedRoute) as jasmine.SpyObj<ActivatedRoute>;
+    mockMedicService = TestBed.inject(MedicService) as jasmine.SpyObj<MedicService>;
+    mockInquiryService = TestBed.inject(InquiryService) as jasmine.SpyObj<InquiryService>;
+    mockToastrService = TestBed.inject(ToastrService) as jasmine.SpyObj<ToastrService>;
+  });
+
+  beforeEach(() => {
+
+    const patient: Patient={
+      id: '22',
+      name: 'test',
+      birthDate: 'test',
+      country: 'test',
+      profilePicture: 'test'
+    }
+    const inquiry: Inquiry = {
+      id: '1',
+      shape: 'Example Shape',
+      numberOfInjuries: '2',
+      distribution: 'Example Distribution',
+      comment: 'Example Comment',
+      image: 'Example Image',
+      creationDate: '2022-02-23T08:00:00.000Z',
+      typeOfInjury: 'Example Type',
+      specialty: 'Example Specialty',
+      assigned: false,
+      diagnosis: 'Initial Diagnosis',
+      injuryQuantity: '',
+      patient: patient
+
+    };
+
+
+    fixture = TestBed.createComponent(InquiryDetailComponent);
+    component = fixture.componentInstance;
+    component.selectedInquiry = inquiry
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('calculateAge', () => {
+    it('should return the age based on birthDate', () => {
+      const age = component.calculateAge('1990-01-01');
+      expect(age).toBe(33);
+    });
+  });
+
+  describe('cancel', () => {
+    it('should emit false to cancelOutput', () => {
+      spyOn(component.cancelOutput, 'emit');
+      component.cancel();
+      expect(component.cancelOutput.emit).toHaveBeenCalledWith(false);
+    });
+  });
+
+
+
+
+
+/* import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -160,3 +269,4 @@ describe('InquiryDetailComponent', () => {
 
 
 });
+ */
