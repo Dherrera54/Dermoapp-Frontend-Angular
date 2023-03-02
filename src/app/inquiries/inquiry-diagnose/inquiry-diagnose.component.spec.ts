ba86cd7e4e3dbc2e8c7119387268ee55c01ef291
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-03-02 18:36:20 -0500

    Updated inquiry detail spes

--- src/app/inquiries/inquiry-diagnose/inquiry-diagnose.component.spec.ts
+++ src/app/inquiries/inquiry-diagnose/inquiry-diagnose.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -12,20 +12,29 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ToastrModule } from 'ngx-toastr';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormBuilder } from '@angular/forms';
+import { InquiryService } from '../inquiry.service';
+import { Inquiry } from '../inquiriy';
+import { Patient } from 'src/app/shared/models/patient';
+import { of } from 'rxjs';
 
 
 describe('InquiryDiagnoseComponent', () => {
   let component: InquiryDiagnoseComponent;
   let fixture: ComponentFixture<InquiryDiagnoseComponent>;
+  let inquiryServiceSpy: jasmine.SpyObj<InquiryService>;
   let debug: DebugElement;
 
 
   beforeEach(async(() => {
+    const spy = jasmine.createSpyObj('InquiryService', ['getInquiryById']);
+
     TestBed.configureTestingModule({
+
       declarations: [ InquiryDiagnoseComponent ],
       imports:[HttpClientTestingModule, RouterTestingModule, ToastrModule.forRoot(), HttpClientModule,SharedModule, TranslateModule.forRoot()],
       providers: [
         FormBuilder,
+        { provide: InquiryService, useValue: spy },
         {
           provide: Router,
           useValue: {
@@ -42,15 +51,55 @@ describe('InquiryDiagnoseComponent', () => {
       ]
     })
     .compileComponents();
+
+    inquiryServiceSpy = TestBed.inject(InquiryService) as jasmine.SpyObj<InquiryService>;
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InquiryDiagnoseComponent);
     component = fixture.componentInstance;
+    component.inquiryId = 'test-inquiry-id';
+    component.token = 'test-token';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should call inquiryService.getInquiryById with correct arguments', fakeAsync(() => {
+    const patient: Patient={
+      id: '22',
+      name: 'test',
+      birthDate: '2000-02-23T08:00:00.000Z',
+      country: 'test',
+      profilePicture: 'test'
+    }
+    const inquiry: Inquiry = {
+      id: '1',
+      shape: 'Example Shape',
+      numberOfInjuries: '2',
+      distribution: 'Example Distribution',
+      comment: 'Example Comment',
+      image: 'Example Image',
+      creationDate: '2022-02-23T08:00:00.000Z',
+      typeOfInjury: 'Example Type',
+      specialty: 'Example Specialty',
+      assigned: false,
+      diagnosis: 'Initial Diagnosis',
+      injuryQuantity: '',
+      patient: patient
+
+    };
+
+
+    inquiryServiceSpy.getInquiryById.and.returnValue(of(inquiry));
+
+    component.getInquiryById();
+
+    expect(inquiryServiceSpy.getInquiryById).toHaveBeenCalledWith('test-inquiry-id', 'test-token');
+
+    // Need to use tick() to simulate asynchronous behavior of subscribe() call
+    tick();
+    expect(component.inquiry).toEqual(inquiry);
+  }));
 });
