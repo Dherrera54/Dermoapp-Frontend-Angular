41d5029918b18ba215d0400ccb2650cbdb9599cb
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-03-01 20:46:38 -0500

    Updated inquiri detail specs

--- src/app/inquiries/inquiry-detail/inquiry-detail.component.spec.ts
+++ src/app/inquiries/inquiry-detail/inquiry-detail.component.spec.ts
@@ -1,44 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+
 
 import { InquiryDetailComponent } from './inquiry-detail.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MedicService } from 'src/app/medic/medic.service';
 import { Inquiry } from '../inquiriy';
-import { of, throwError } from 'rxjs';
+import { of } from 'rxjs';
 import { Patient } from 'src/app/shared/models/patient';
-import { Router } from '@angular/router';
 
 describe('InquiryDetailComponent', () => {
   let component: InquiryDetailComponent;
   let fixture: ComponentFixture<InquiryDetailComponent>;
-  let medicServiceSpy: jasmine.SpyObj<MedicService>;
   let medicService: MedicService;
 
   beforeEach(async(() => {
-    medicService = TestBed.inject(MedicService);
+
 
     const spy = jasmine.createSpyObj('MedicService', ['addInquiryToMedic']);
     TestBed.configureTestingModule({
       declarations: [ InquiryDetailComponent ],
       imports:[ HttpClientTestingModule, RouterTestingModule, ToastrModule.forRoot(), HttpClientModule,SharedModule, TranslateModule.forRoot()],
       providers: [ { provide: MedicService, useValue: spy },
-        {provide: Router,
-        useValue: {
-          navigate: jasmine.createSpy('navigate'),
-        }},
-        {provide: ToastrService},
+               {provide: ToastrService},
 ]
     })
     .compileComponents();
 
-    medicServiceSpy = TestBed.inject(MedicService) as jasmine.SpyObj<MedicService>;
+
   }));
 
   beforeEach(() => {
@@ -61,23 +54,6 @@ describe('InquiryDetailComponent', () => {
     expect(age).toEqual(0);
   });
 
-  it('should call addInquiryToMedic with correct parameters', () => {
-    const token = 'test_token';
-    const medicId = 'test_medic_id';
-    const inquiryId = 'test_inquiry_id';
-    component.token = token;
-    component.medicId = medicId;
-    component.selectedInquiry = { id: inquiryId } as Inquiry;
-    const successSpy = spyOn(component, 'showSuccess');
-    const errorSpy = spyOn(component, 'showError');
-    medicServiceSpy.addInquiryToMedic.and.returnValue(of(null));
-
-    component.claim();
-
-    expect(medicServiceSpy.addInquiryToMedic).toHaveBeenCalledWith(medicId, token, inquiryId);
-    expect(successSpy).toHaveBeenCalled();
-    expect(errorSpy).not.toHaveBeenCalled();
-  });
 
   it('should set owned to true if inquiry belongs to medic', () => {
 
