80885befead4b8f9b08b7b047e2266f6bc622682
Dario_herrera <d.herrerag@uniandes.edu.co>
2023-03-08 20:50:02 -0500

    update to medic inquiry list spec

--- src/app/inquiries/inquiry-medic-list/inquiry-medic-list.component.spec.ts
+++ src/app/inquiries/inquiry-medic-list/inquiry-medic-list.component.spec.ts
@@ -9,7 +9,7 @@ import {RouterTestingModule} from '@angular/router/testing';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
-import { ToastrModule } from 'ngx-toastr';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 
@@ -17,12 +17,14 @@ describe('InquiryMedicListComponent', () => {
   let component: InquiryMedicListComponent;
   let fixture: ComponentFixture<InquiryMedicListComponent>;
   let debug: DebugElement;
+  let toastrServiceSpy: jasmine.SpyObj<ToastrService>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ InquiryMedicListComponent ],
       imports:[HttpClientTestingModule, RouterTestingModule, ToastrModule.forRoot(), HttpClientModule,SharedModule, TranslateModule.forRoot()],
       providers: [
+        {provide: ToastrService, useValue: toastrServiceSpy},
         {
           provide: Router,
           useValue: {
@@ -72,5 +74,37 @@ describe('InquiryMedicListComponent', () => {
     component.scrollHorizontally(val);
 
     expect(mediaScroller.scrollLeft).toBe(-scrollAmount * val);
+  });
+  it('should calculate age correctly for a given birth date', () => {
+    const birthDate = '1990-01-01';
+    const age = component.calculateAge(birthDate);
+    let timeDiff = Math.abs(Date.now() - new Date(birthDate).getTime());
+    let ageExpect = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    expect(age).toEqual(ageExpect);
+  });
+
+  it('should return 0 when birth date is not provided', () => {
+    const age = component.calculateAge('');
+    expect(age).toEqual(0);
+  });
+  it('should call error method of toastr service with correct parameters', () => {
+    const errorMsg = 'An error occurred';
+    component.showError(errorMsg);
+    expect(toastrServiceSpy.error).toHaveBeenCalledWith(errorMsg, 'Error!');
+  });
+  it('should set selected to true on cancel', () => {
+    const cancel = true;
+
+    component.onCancel(cancel);
+
+    expect(component.selected).toBeTrue();
+  });
+
+  it('should call getInquiriesFromMedic on claimed inquiry', () => {
+    spyOn(component, 'getInquiriesFromMedic');
+
+    component.onClaimedInquiry(true);
+
+    expect(component.getInquiriesFromMedic).toHaveBeenCalled();
   });
 });
